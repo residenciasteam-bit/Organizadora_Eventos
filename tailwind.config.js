d11e5d22781821e7ac69d8c9b9d33c4388bf8783
Daniel Alcazar <residenciasteam@gmail.com>
2025-08-15 00:02:11 -0600

    Primer commit

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
+  theme: { extend: {} },
+  darkMode: "class",
+  plugins: [],
+}
